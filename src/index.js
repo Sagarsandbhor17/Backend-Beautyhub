90ea73522c49967110706abd25ccb01cbf8413f6
Mayur-Pc <sagar12500@gmail.com>
2022-11-13 16:38:05 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const dotenv=require("dotenv");
 var bodyParser = require("body-parser");
 const cors=require("cors");
 const user=require('./features/users/users.router');
 const product=require('./features/products/products.router');
 const cart=require('./features/cart/cart.router');
 const dbConnect=require('./config/db')
+dotenv.config();
 let PORT =8080;
 
 const app = express();
